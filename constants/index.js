07a549a3085b028277d87f4e2496ef5f46a31c68
AmoghAtreya <amoghatreya100@gmail.com>
2024-09-09 20:42:57 -0700

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'Lorem ipsum',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'Lorem ipsum dolor',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Lorem ipsum',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Lorem ipsum',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Lorem ipsum',
+  },
+];
+
+export const startingFeatures = [
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    subtitle:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    subtitle:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    subtitle:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
